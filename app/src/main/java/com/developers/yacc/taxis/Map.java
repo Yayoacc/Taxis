1a769158f18274c2e9a7d32f500aa69e23b01812
Yacc <yayoacc_96@hotmail.com>
2018-06-13 15:36:13 -0500

    Second Commit

--- app/src/main/java/com/developers/yacc/taxis/Map.java
+++ app/src/main/java/com/developers/yacc/taxis/Map.java
@@ -1,9 +1,9 @@
 package com.developers.yacc.taxis;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
@@ -13,8 +13,10 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -26,6 +28,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationListener;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -36,18 +39,36 @@ import java.sql.Statement;
 public class Map extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        com.google.android.gms.location.LocationListener {
+        LocationListener {
+    private static final int MY_PERMISSION_REQUEST_CODE = 7171;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 7172;
+    private boolean mRequestingLocationUpdates = false;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
+    private static int UPDATE_INTERVAL = 5000; // SEC
+    private static int FATEST_INTERVAL = 3000; // SEC
+    private static int DISPLACEMENT = 10; // METERS
     private GoogleMap mMap;
-    GoogleApiClient mGogleApiClient;
-    Location mLastLocation;
-    LocationRequest mLocationRequest;
     Marker mCurrent;
     LatLng Lantlong;
     Connection con;
     String Lat, Long;
     String un, passw, db, ip, value, valuet;
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayServices()) {
+                        buildGoogleApiClient();
+                        createLocationRequest();
+                    }
+                }
+                break;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ip = "208.118.63.49";
@@ -60,6 +81,19 @@ public class Map extends FragmentActivity implements OnMapReadyCallback,
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Run-time request permission
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, MY_PERMISSION_REQUEST_CODE);
+        } else {
+            if (checkPlayServices()) {
+                buildGoogleApiClient();
+                createLocationRequest();
+            }
+        }
     }
 
 
@@ -81,62 +115,132 @@ public class Map extends FragmentActivity implements OnMapReadyCallback,
         else {
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
+            displayLocation();
+            tooglePeriodicLoctionUpdates();
         }
        /* mMap.moveCamera(CameraUpdateFactory.newLatLng(Lantlong));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
         mCurrent = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_car))
                 .position(Lantlong)
                 .title("Tu"));*/
-
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
-        mGogleApiClient.connect();
     }
     @Override
     public void onLocationChanged(Location location) {
-        mLastLocation = location;
+            mLastLocation = location;
+            displayLocation();
         Lantlong = new LatLng(location.getLatitude(), location.getLongitude());
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Lantlong));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
-        /*mCurrent = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_car))
+        Toast.makeText(Map.this, ""+Lantlong, Toast.LENGTH_LONG).show();
+        mCurrent = mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_car))
                 .position(Lantlong)
-                .title("Tu"));*/
+                .title("Tu"));
         double lat = location.getLatitude();
         double lon = location.getLongitude();
         Lat = Double.toString(lat);
         Long = Double.toString(lon);
+        //tooglePeriodicLoctionUpdates();
         Update update = new Update();
         update.execute("");
     }
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGogleApiClient, mLocationRequest, this);
+        displayLocation();
+        if(mRequestingLocationUpdates)
+            startLocationUpdates();
     }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
     @Override
-    protected void onStop() {
-        super.onStop();
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGogleApiClient, this);
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mGoogleApiClient != null)
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+        if(mGoogleApiClient != null)
+            mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    private void tooglePeriodicLoctionUpdates() {
+        if(!mRequestingLocationUpdates){
+            mRequestingLocationUpdates = true;
+            startLocationUpdates();
+        }
+        else {
+            mRequestingLocationUpdates = false;
+            stopLocationUpdates();
+        }
+    }
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+        } else {
+            Toast.makeText(Map.this, "Couldn't get the location. Make sure location is enable on the device", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+
+    }
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        //Fix first time run app if permission doesn't grant yet so can't get anything
+        mGoogleApiClient.connect();
+    }
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "This device is not supported", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+    private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+    private void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
     @SuppressLint("NewApi")
     public Connection connectionclass(String user, String password, String database, String server) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -173,7 +277,8 @@ public class Map extends FragmentActivity implements OnMapReadyCallback,
                     if (con == null) {
                         z = "Check Your Internet Access!";
                     } else {
-                        String query = "INSERT INTO ubicacion VALUES("+Lat+", "+Long+");";
+
+                        String query = "UPDATE ubicacion SET latitud = '"+Lat+"', longuitud = '"+Long+"'  WHERE id = 'Yacc_99'  ("+Lat+", "+Long+");";
                         Statement stmt = con.createStatement();
                         stmt.executeQuery(query);
                         con.close();
